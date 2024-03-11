f4fd31ce16caa11f1bd6a617bdd37af9653572e8
Matheus <msousa@sjtu.edu.cn>
2024-03-11 15:05:40 +0800

    test: Binomial

--- test/test-binomial.cpp
+++ test/test-binomial.cpp
@@ -9,13 +9,13 @@ using testing::UnorderedElementsAre;
 
 // https://oeis.org/A000217
 TEST(test_binomial, triangle_numbers) {
-  EXPECT_EQ(binomial(2,2),1);
-  EXPECT_EQ(binomial(3,2),3);
-  EXPECT_EQ(binomial(4,2),6);
-  EXPECT_EQ(binomial(5,2),10);
-  EXPECT_EQ(binomial(6,2),15);
-  EXPECT_EQ(binomial(7,2),21);
-  EXPECT_EQ(binomial(8,2),28);
-  EXPECT_EQ(binomial(9,2),36);
-  EXPECT_EQ(binomial(10,2),45);
+  EXPECT_EQ(binomial(2, 2), 1);
+  EXPECT_EQ(binomial(3, 2), 3);
+  EXPECT_EQ(binomial(4, 2), 6);
+  EXPECT_EQ(binomial(5, 2), 10);
+  EXPECT_EQ(binomial(6, 2), 15);
+  EXPECT_EQ(binomial(7, 2), 21);
+  EXPECT_EQ(binomial(8, 2), 28);
+  EXPECT_EQ(binomial(9, 2), 36);
+  EXPECT_EQ(binomial(10, 2), 45);
 }
